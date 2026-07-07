be85ec92f77b81988f0fa2135adda9a0e553db6f
codehub-py <imvirenjalamkar2005@gmail.com>
2026-07-08 00:04:09 +0530

    Data Types In Python

---
+++ Python/datatypes.py
@@ -0,0 +1,44 @@
+#DataTypes - String,Number,Tuples,Lists,Dictionaries,sets,Frozenset
+
+#Mutable - Lists,sets and Dictionaries
+
+#Immutable - String,Number,tuples and Frozenset
+
+#DataType Declaration
+
+ #String
+vars = "String"
+print(vars)
+print(type(vars))
+
+ #Number
+varin = 22
+varfn = 12.5
+print(varin)
+print(type(varin))
+print(varfn)
+print(type(varfn))
+
+ #Tuples
+vart = ("Name","Age")
+print(vart)
+print(type(vart))
+ #lists
+varl = ["Name","Age"]
+print(varl)
+print(type(varl))
+
+ #Dictionary
+vard = {"Name":"Viren","Age":22}
+print(vard)
+print(type(vard))
+
+ #Sets
+varst = {1,2,3}
+print(varst)
+print(type(varst))
+
+ #Frozenset
+varfst = frozenset([1,2,3])
+print(varfst)
+print(type(varfst))
