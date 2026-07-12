f98a8b09525105fdf8ec7abb7dd95d79d1bf6018
codehub-py <imvirenjalamkar2005@gmail.com>
2026-07-12 23:44:22 +0530

    Python While Loop

---
+++ Python/whileloop.py
@@ -0,0 +1,31 @@
+x = 0
+y = "spiderman"
+
+while x < len(y):
+    if y[x] == "i":
+        x += 1
+        continue
+
+    print(y[x])
+    x += 1
+#----------------------------------------------------------------
+x = 0
+y = "ironman"
+
+while x < len(y):
+    if y[x] == "i":
+        x += 1
+        break
+    print("value of x:", y[x])
+    x += 1
+
+#-----------------------------------------------------------------
+x=0
+y=["spiderman","ironman","halk","Thor","captainamerica"]
+
+while x < len(y):
+    if y[x]== "tushar" :
+       x+=1
+       continue
+    print("value of x:",y[x])
+    x+=1
